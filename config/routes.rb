dde1bad11c77c744952ea9cc2b979c1b4d252b16
Elsa Lebas <lebas.elsa@gmail.com>
2020-11-10 16:28:39 +0100

    refacto with _form file for new and edit + refacto routes with resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
+#REFACTO
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/tasks", to: "tasks#index" # READ all
-  get "tasks/new", to: "tasks#new" # UPDATE display form
-  get "/tasks/:id", to: "tasks#show", as: :task # READ one specific
-  post "/tasks", to: "tasks#create"
-  patch "/tasks/:id", to: "tasks#update" #UPDATE input from user to change in data base
-  get "/tasks/:id/edit", to: "tasks#edit", as: :edit_task # UPDATE get the task to update / display form
-  delete "tasks/:id", to: "tasks#destroy" # DELETE destroy a specific task
+  resources :tasks
 end
+
+# Rails.application.routes.draw do
+#   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+#   get "/tasks", to: "tasks#index" # READ all
+#   post "/tasks", to: "tasks#create"
+#   get "tasks/new", to: "tasks#new", as: :new_task # UPDATE display form
+#   get "/tasks/:id/edit", to: "tasks#edit", as: :edit_task # UPDATE get the task to update / display form
+#   get "/tasks/:id", to: "tasks#show", as: :task # READ one specific
+#   patch "/tasks/:id", to: "tasks#update" #UPDATE input from user to change in data base
+#   delete "tasks/:id", to: "tasks#destroy" # DELETE destroy a specific task
+# end
